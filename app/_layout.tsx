911043cc2362c39f2b70419f1ac502abd18e0c1d
kashaf12 <kashafaahmed@gmail.com>
2024-05-23 00:43:22 +0530

    feat: added splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,33 @@
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-export default function RootLayout() {
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function ApplicationNavigator() {
+  const [isAppReady, setAppReady] = useState(false);
+  useEffect(() => {
+    if (isAppReady) {
+      SplashScreen.hideAsync();
+    } else {
+      //TODO: this should be removed
+      setTimeout(() => setAppReady(true), 2000);
+    }
+  }, [isAppReady]);
+
+  if (!isAppReady) {
+    return null;
+  }
+
+  return <MainNavigationStack />;
+}
+
+function MainNavigationStack() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
